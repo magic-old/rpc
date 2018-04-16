4dd327512a70d6f84b9025aaa43b3932b6b8b17f
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-16 02:55:41 +0200

    fix server tests

--- test/server.js
+++ test/server.js
@@ -1,36 +1,42 @@
+const { promise } = require('@magic/test')
+
 const { client, server } = require('../src')
 
-const startServer = () => {
+const startServer = (port = 50051) => () => {
   const actions = {
     Hello: (call, cb) => {
       const { user = 'world' } = call.request
       cb(null, { message: `Hello ${user}` })
     },
   }
+  const app = server(actions, { port })
 
-  const app = server({ actions })
-  // console.log('server started')
-
+  console.log('server started on', { port })
   return () => {
     app.forceShutdown()
-    // console.log('server closed')
   }
 }
 
 const fns = [
   {
     fn: () => new Promise(r => client().Hello({}, error => r(error))),
-    before: startServer,
+    before: startServer(),
     expect: null,
   },
   {
-    fn: () => new Promise(r => client().Hello({}, (error, data) => r(data))),
-    before: startServer,
+    fn: () =>
+      new Promise(r =>
+        client({ port: 232323 }).Hello({}, (error, data) => r(data)),
+      ),
+    before: startServer(232323),
     expect: ({ message }) => message === 'Hello world',
   },
   {
-    fn: () => new Promise(r => client().Hello({}, (error, data) => r(data))),
-    before: startServer,
+    fn: () =>
+      new Promise(r =>
+        client({ port: 232324 }).Hello({}, (error, data) => r(data)),
+      ),
+    before: startServer(232324),
     expect: ({ message }) => message === 'Hello world',
   },
 ]
